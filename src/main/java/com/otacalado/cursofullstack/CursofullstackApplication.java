924988b471c6978301fb5bd70ad98b8e754fba4e
otacalado <otavio.calado@fatec.sp.gov.br>
2024-02-09 18:16:53 -0300

    Projeto criado

---
+++ src/main/java/com/otacalado/cursofullstack/CursofullstackApplication.java
@@ -0,0 +1,13 @@
+package com.otacalado.cursofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursofullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursofullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otacalado/cursofullstack/CursofullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otacalado.cursofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursofullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
